69424692ad974b5f2bb493d3ba92125b0cd87fbc
Steven Ndegwa <stevencallistus19@gmail.com>
2023-10-04 14:29:12 +0300

    feat: add users route

--- server/app/index.js
+++ server/app/index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 
 const productsRoute = require("./routes/products") 
-
+const usersRoute = require("./routes/users")
 
 app.use("/products", productsRoute)
+app.use("/users", usersRoute);
 
 
 app.listen(3000, ()=> {console.log(`server started on port 3000`)})
