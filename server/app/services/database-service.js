273a60b3c0511ca795c7b2c4249fff42923ddfb2
Steven Ndegwa <stevencallistus19@gmail.com>
2023-10-11 15:49:40 +0300

    refractor: move database modification features to models

--- server/app/services/database-service.js
+++ server/app/services/database-service.js
@@ -11,48 +11,7 @@ const pool = mysql.createPool({
 });
 
 class Database {
-  static Products() {
-    return {
-      find: () => {
-        return {
-          all: async () => {
-            let results =  await Database.query(`SELECT * FROM products`);
-            return results;
-          },
-          where: async (column, value) => {
-            let results =  await Database.query(
-              `SELECT * FROM products WHERE ${column} = "${value}"`
-            );
-            return results;
-          },
-        };
-      },
-
-      add: async (columns, values) => {
-        let results =  await Database.query(
-          `INSERT INTO products(${columns}) VALUES (${values})`
-        );
-        return results;
-      },
-
-      update: async (columnToEdit, newValue, column, value) => {
-        let results =  await Database.query(
-          `UPDATE products SET ${columnToEdit} = "${newValue}" WHERE ${column} = "${value}"`
-        );
-        return results;
-      },
-
-      remove: async (column, value) => {
-        let results =  await Database.query(
-          `DELETE FROM products WHERE  ${column} = "${value}"`, 
-        );
-        return results;
-      },
-    };
-  }
-
-  static query(query) {
-    
+    static query(query) {
     return new Promise((resolve, reject) => {
       pool.getConnection((err, connection) => {
         if (err) reject(err);
