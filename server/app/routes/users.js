2d64e80de64e81cb55bca6b708dd22851e1d7067
Steven Ndegwa <stevencallistus19@gmail.com>
2023-10-11 15:55:23 +0300

    refractor: change name of request parameter

--- server/app/routes/users.js
+++ server/app/routes/users.js
@@ -1,6 +1,11 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
+const UsersController = require("../controllers/users");
 
-
+router.get("/", UsersController.getAllUsers)
+router.get("/:credential", UsersController.getUser)
+router.post("/", UsersController.addUser)
+router.put("/:id", UsersController.updateUser)
+router.delete("/:id", UsersController.deleteUser)
 
 module.exports = router;
