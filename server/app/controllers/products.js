fea0085e75546c82d22a4093d40889753f71d03f
Steven Ndegwa <stevencallistus19@gmail.com>
2023-10-11 15:58:49 +0300

    feat: add feature to search products

--- server/app/controllers/products.js
+++ server/app/controllers/products.js
@@ -1,27 +1,34 @@
-const Database = require("../services/database-service")
-const productModel = require("../models/product-model")
+const ProductModel = require("../models/product-model")
 
 class ProductsController{
-    static getAllProducts  = async(req,res)=>{
-        const results = await Database.Products().find().all()
+    static async getAllProducts(req,res,next){
+        const results = await ProductModel.getAllProducts();
         return res.json(results);
     }
-    static getSpecificProduct  = async(req,res)=>{
-        const result = await Database.Products().find().where("id", req.params.id)
+    static async getSpecificProduct(req,res,next){
+        const result = await ProductModel.getProduct(req.params.credential); // either name or id
         return res.json(result);
     }
-    static addProduct = async(req,res)=>{
-        const product = new productModel(); /// values
-        const results = await Database.Products().add(product.columns, product.values)
+    static async getSearchProducts(req,res,next){
+        const result = await ProductModel.getSearchProducts(req.params.searchValue)
+        return res.json(result);
+    }
+    static async addProduct(req,res,next){
+        const name = await ProductModel.getProduct(req.body.name);
+        if(name.length) return res.json("Product already exists in the database");
+
+        const Product = new ProductModel(req.body.name, req.body.description, req.body.supplierId, req.body.price, req.body.stock);
+        const results = await Product.addProduct();
         return res.json(results);
     }
-    static updateProduct  = async(req,res)=>{
-        const results = await Database.Products().update("name", "newName", "id", req.params.id)
+    static async updateProduct(req,res,next){
+        const Product = new ProductModel(req.body.name, req.body.description, req.body.supplierId, req.body.price, req.body.stock);
+        const results = await Product.updateProduct(req.params.id);
         return res.json(results);
     }
-    static deleteProduct  = async(req,res)=>{
-        const results = await Database.Products().remove("id", req.params.id)
-        return res.json(results);
+    static async deleteProduct(req,res,next){
+        const result = await ProductModel.getProduct(req.params.id);
+        return res.json(result);
     }
 }
 
