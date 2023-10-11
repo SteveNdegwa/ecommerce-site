5e23c11de393ac84ed0bb92731d1becb15d20989
Steven Ndegwa <stevencallistus19@gmail.com>
2023-10-11 15:51:59 +0300

    feat: add login and suppliers routes

--- server/app/index.js
+++ server/app/index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 const productsRoute = require("./routes/products") 
 const usersRoute = require("./routes/users")
+const suppliersRoute = require("./routes/suppliers")
+const loginRoute = require("./routes/login")
 
 app.use("/products", productsRoute)
 app.use("/users", usersRoute);
+app.use("/suppliers", suppliersRoute);
+app.use("/login", loginRoute);
 
 
 app.listen(3000, ()=> {console.log(`server started on port 3000`)})
