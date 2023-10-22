c4d8bac1facd30aa7526d1841fb1463ead6fa4e9
Steven Ndegwa <stevencallistus19@gmail.com>
2023-10-22 16:55:36 +0300

    refractor: change login route to jwt route

--- server/app/index.js
+++ server/app/index.js
@@ -3,19 +3,23 @@ const app = express();
 
 const cors = require("cors")
 
-app.use(cors())
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods:['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD'],
+    credentials: true,
+}))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 const productsRoute = require("./routes/products") 
 const usersRoute = require("./routes/users")
 const suppliersRoute = require("./routes/suppliers")
-const loginRoute = require("./routes/login")
+const jwtRoute = require("./routes/jwt")
 
 app.use("/products", productsRoute)
 app.use("/users", usersRoute);
 app.use("/suppliers", suppliersRoute);
-app.use("/login", loginRoute);
+app.use("/jwt", jwtRoute);
 
 
 app.listen(3000, ()=> {console.log(`server started on port 3000`)})
