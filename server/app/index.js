565a27b6d4c7b2e41fe1f38283e228068af882bf
Steven Ndegwa <stevencallistus19@gmail.com>
2023-10-24 17:14:45 +0300

    feat: add cookie-parser middleware

--- server/app/index.js
+++ server/app/index.js
@@ -2,14 +2,17 @@ const express = require("express");
 const app = express();
 
 const cors = require("cors")
-
 app.use(cors({
     origin: "http://localhost:5173",
     methods:['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD'],
     credentials: true,
 }))
+
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 
 const productsRoute = require("./routes/products") 
 const usersRoute = require("./routes/users")
@@ -22,4 +25,4 @@ app.use("/suppliers", suppliersRoute);
 app.use("/jwt", jwtRoute);
 
 
-app.listen(3000, ()=> {console.log(`server started on port 3000`)})
+app.listen(3000, ()=> console.log(`server started on port 3000`));
