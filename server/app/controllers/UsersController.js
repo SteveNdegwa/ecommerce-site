7ffb38994b9d1ad24fb6d90e8738e57d3be78f7a
Steven Ndegwa <stevencallistus19@gmail.com>
2023-11-01 05:08:14 +0300

    feat: add error handling for server errors

--- server/app/controllers/UsersController.js
+++ server/app/controllers/UsersController.js
@@ -2,50 +2,71 @@ const UserModel = require("../models/UserModel");
 
 class UsersController {
   static async getAllUsers(req, res, next) {
-    const result = await UserModel.getAllUsers();
-    return res.json(result);
+    try {
+      const result = await UserModel.getAllUsers();
+      return res.json(result);
+    } catch (error) {
+      return res.status(500).json("Server error")
+    }
   }
 
   static async getUser(req, res, next) {
-    const result = await UserModel.getUser(req.params.credential);
-    return res.json(result);
+    try {
+      const result = await UserModel.getUser(req.params.credential);
+      return res.json(result);
+    } catch (error) {
+      return res.status(500).json("Server error")
+    }
   }
 
   static async addUser(req, res, next) {
-    const email = await UserModel.getUser(req.body.email);
-    if (email.length) return res.status(200).json("The email already exists");
+    try {
+      const email = await UserModel.getUser(req.body.email);
+      if (email.length) return res.status(200).json("The email already exists");
+  
+      const username = await UserModel.getUser(req.body.username);
+      if (username.length) return res.status(200).json("The username is already taken");
+  
+      const User = new UserModel(
+        req.body.username,
+        req.body.email,
+        req.body.password
+      );
+      const result = await User.addUser();
+      return res.status(201).json(result);
 
-    const username = await UserModel.getUser(req.body.username);
-    if (username.length) return res.status(200).json("The username is already taken");
-
-    const User = new UserModel(
-      req.body.username,
-      req.body.email,
-      req.body.password
-    );
-    const result = await User.addUser();
-    return res.status(201).json(result);
+    } catch (error) {
+      return res.status(500).json("Server error")
+    }
   }
 
   static async updateUser(req, res, next) {
-    const email = await UserModel.getOtherUser(req.body.email, req.user.user_id);
-    if (email.length) return res.status(200).json("The email already exists");
-
-    const username = await UserModel.getOtherUser(req.body.username, req.user.user_id);
-    if (username.length) return res.status(200).json("The username is already taken");
-
-    const User = new UserModel(
-      req.body.username,
-      req.body.email,
-      req.body.password
-    );
-    const result = await User.updateUser(req.params.id);
-    return res.status(201).json(result);
+    try {
+      const email = await UserModel.getOtherUser(req.body.email, req.user.user_id);
+      if (email.length) return res.status(200).json("The email already exists");
+  
+      const username = await UserModel.getOtherUser(req.body.username, req.user.user_id);
+      if (username.length) return res.status(200).json("The username is already taken");
+  
+      const User = new UserModel(
+        req.body.username,
+        req.body.email,
+        req.body.password
+      );
+      const result = await User.updateUser(req.params.id);
+      return res.status(201).json(result);
+    } catch (error) {
+      return res.status(500).json("Server error")
+    }
   }
 
   static async deleteUser(req, res, next) {
-    const result = await UserModel.deleteUser(req.params.id);
-    return res.json(result);
+   try {
+     const result = await UserModel.deleteUser(req.params.id);
+     return res.json(result);
+   } catch (error) {
+    return res.status(500).json("Server error")
+   }
   }
 }
 
