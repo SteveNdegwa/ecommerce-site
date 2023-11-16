65f5d740556d11ab352db3137ce3af6751ffbfd4
Steven Ndegwa <stevencallistus19@gmail.com>
2023-11-16 12:00:15 +0300

    refractor: changing revoke-user-token route's parameter to 'username'

--- server/app/routes/jwt.js
+++ server/app/routes/jwt.js
@@ -4,7 +4,7 @@ const JwtController = require("../controllers/JwtController");
 router.post("/get-token", JwtController.getToken);
 router.post("/verify-token", JwtController.verifyToken);
 router.post("/refresh-token", JwtController.refreshToken);
-router.post("/revoke-user-token/:credential",JwtController.revokeUserToken);
+router.post("/revoke-user-token/:username",JwtController.revokeUserToken);
 router.post("revoke-refresh-tokens", JwtController.revokeAllRefreshTokens);
 
 module.exports = router;
