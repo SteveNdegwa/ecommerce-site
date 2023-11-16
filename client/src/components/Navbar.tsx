c348b0483424538df4a83ebf6c10e24efd3e0419
Steven Ndegwa <stevencallistus19@gmail.com>
2023-11-17 00:35:46 +0300

    feat: add logout functionality

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,7 +1,24 @@
+import { useNavigate } from "react-router-dom"
+import { State } from "../store";
+import { useSelector } from "react-redux";
+import api from "../config/api";
+
+
 export function Navbar(){
+  
+    const navigate = useNavigate();
+
+    const username = useSelector((state: State)=> state.user.value.username);
+
+    function onLogout(){
+        api.post("/jwt/revoke-user-token/username")
+        navigate("/login");
+    }
+
     return(
         <div className="navbar">
-            
+            <h2 className="username">{username && <>Hi {username}</>}</h2>
+            {username && <button type="button" onClick={onLogout}>Logout</button>}
         </div>
     )
 }
