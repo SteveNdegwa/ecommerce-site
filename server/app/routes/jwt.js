716a21b7e3fa28fcb3c2941a8086ce3ea9d5278b
Steven Ndegwa <stevencallistus19@gmail.com>
2023-11-21 08:48:36 +0300

    feat: add verifyToken middleware to revoke token routes

--- server/app/routes/jwt.js
+++ server/app/routes/jwt.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
 const JwtController = require("../controllers/JwtController");
+const TokenController = require("../controllers/JwtController");
+
 
 router.post("/get-token", JwtController.getToken);
 router.post("/verify-token", JwtController.verifyToken);
 router.post("/refresh-token", JwtController.refreshToken);
-router.post("/revoke-user-token/:username",JwtController.revokeUserToken);
-router.post("revoke-refresh-tokens", JwtController.revokeAllRefreshTokens);
+router.post("/revoke-user-token/:username",TokenController.verifyToken, JwtController.revokeUserToken);
+router.post("revoke-refresh-tokens",TokenController.verifyToken, JwtController.revokeAllRefreshTokens);
 
 module.exports = router;
