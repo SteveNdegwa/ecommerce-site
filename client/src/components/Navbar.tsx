8903e7faaa4db20b946c0672b7a49d4dbc019c53
Steven Ndegwa <stevencallistus19@gmail.com>
2023-11-30 20:52:12 +0300

    feat: add links

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,6 +1,6 @@
-import { useNavigate } from "react-router-dom"
-import { State } from "../store";
-import { useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom"
+import { State, logout } from "../store";
+import { useDispatch, useSelector } from "react-redux";
 import api from "../config/api";
 
 
@@ -10,15 +10,24 @@ export function Navbar(){
 
     const username = useSelector((state: State)=> state.user.value.username);
 
+    const dispatch = useDispatch();
+    
     function onLogout(){
-        api.post("/jwt/revoke-user-token/username")
+        api.post(`/jwt/revoke-user-token/${username}`)
+        dispatch(logout());
         navigate("/login");
     }
 
     return(
-        <div className="navbar">
+        <nav className="navbar">
+            <div className="links">
+                <Link className="link" to="/">Home</Link>
+                <Link className="link" to="/">Catalog</Link>
+                <Link className="link" to="/">Collections</Link>
+                <Link className="link" to="/">Contact us</Link>
+            </div>
             <h2 className="username">{username && <>Hi {username}</>}</h2>
-            {username && <button type="button" onClick={onLogout}>Logout</button>}
-        </div>
+            <button type="button" onClick={onLogout}>{username ? <>Logout</> : <>Login</>}</button>
+        </nav>
     )
 }
