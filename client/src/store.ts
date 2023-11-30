a51e0493705ba1fc10959f13ef33b0586c60c079
Steven Ndegwa <stevencallistus19@gmail.com>
2023-11-30 20:48:41 +0300

    feat: add role in values of user's state

--- client/src/store.ts
+++ client/src/store.ts
@@ -3,20 +3,21 @@ import {configureStore, createSlice} from "@reduxjs/toolkit"
 export interface State{
     user: {
         value:{
-            username: string
+            username: string,
+            role: string,
         }
     }
 }
 
 const userSlice = createSlice({
     name: "user",
-    initialState: {value:{username: null}},
+    initialState: {value:{username: null, role: null}},
     reducers:{
         login:(state, action)=>{
             state.value = action.payload
         },
         logout:(state)=>{
-            state.value = {username: null}
+            state.value = {username: null, role: null}
         }
     }
 })
