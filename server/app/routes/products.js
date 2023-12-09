410833aec82a5e620dc1962441670b3e6e376ced
Steven Ndegwa <stevencallistus19@gmail.com>
2023-12-09 21:04:51 +0300

    feat: only verify token when adding, deleting or updating product

--- server/app/routes/products.js
+++ server/app/routes/products.js
@@ -3,13 +3,12 @@ const router = express.Router();
 const TokenController = require("../controllers/JwtController");
 const ProductsController = require("../controllers/ProductsController");
 
-router.use(TokenController.verifyToken);
 
 router.get("/", ProductsController.getAllProducts);
-router.get("/:credential", ProductsController.getSpecificProduct);
+router.get("/:id", ProductsController.getProductById);
 router.get("/search/:searchValue", ProductsController.getSearchProducts);
-router.post("/", ProductsController.addProduct);
-router.put("/:id", ProductsController.updateProduct);
-router.delete("/:id", ProductsController.deleteProduct);
+router.post("/", TokenController.verifyToken, ProductsController.addProduct);
+router.put("/:id", TokenController.verifyToken, ProductsController.updateProduct);
+router.delete("/:id", TokenController.verifyToken, ProductsController.deleteProduct);
 
 module.exports = router;
