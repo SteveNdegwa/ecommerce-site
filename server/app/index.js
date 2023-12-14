dd7f588f002d9cc4ec683145f657f5b0dfb032e8
Steven Ndegwa <stevencallistus19@gmail.com>
2023-12-15 02:26:57 +0300

    feat: add 'DELETE' to CORS methods

--- server/app/index.js
+++ server/app/index.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require("cors")
 app.use(cors({
     origin: "http://localhost:5173",
-    methods:['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD'],
+    methods:['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD', 'DELETE'],
     credentials: true,
 }))
 
